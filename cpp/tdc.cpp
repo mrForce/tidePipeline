a80e6a03813b1d3260fa653ad4db42a21a54c8b4
Jordan Force <jordan.force@uconn.edu>
2018-03-30 15:55:54 -0400

    Finished program. Now need to test

--- cpp/tdc.cpp
+++ cpp/tdc.cpp
@@ -9,6 +9,7 @@ struct match_t{
   std::string peptide;
   double score;
   double q_value;
+  match_t(std::string peptide_, double score_)  {peptide = peptide_; score = score_; q_value = 0;}
   bool operator<(const match_t& match) const {
     return score < match.score;
   }
@@ -19,8 +20,8 @@ void tdc_procedure(std::vector<match_t> targets, std::vector<match_t> decoys){
   std::sort(targets.begin(), targets.end());
   std::sort(decoys.begin(), decoys.end());
   double min_fpr = 1.0;
-  size_t num_targets = targets.length();
-  size_t num_decoys = decoys.length();
+  size_t num_targets = targets.size();
+  size_t num_decoys = decoys.size();
   //this is the number of match_t objects before decoy_iter in the decoys vector
   size_t decoy_iter_num = 0;
   std::vector<match_t>::iterator decoy_iter = decoys.begin();
@@ -57,12 +58,36 @@ int main(int argc, char* argv[]){
   }
   std::vector<match_t> targets;
   std::vector<match_t> decoys;
-  std::ifstream inFile;
-  inFile.open(argv[1]);
-  std::ofstream outFile;
-  outFile.open(argv[2]);
-  std::string len_string(argv[3]);
-  int len = std::stoi(len_string, nullptr);
+
+
+  std::ifstream targetFile;
+  targetFile.open(argv[1]);
+  std::ifstream decoyFile;
+  decoyFile.open(argv[2]);
+  std::string line;
+  size_t tab_location;
+  std::string peptide;
+  if(targetFile.is_open() && decoyFile.is_open()){
+    while(std::getline(targetFile, line)){
+      if(line.length() > 0){
+        tab_location = line.find("\t");
+	peptide = line.substr(0, tab_location);
+	targets.emplace_back(peptide, std::stod(line.substr(tab_location + 1, std::string::npos)));
+      }
+    }
+    while(std::getline(decoyFile, line)){
+      if(line.length() > 0){
+	tab_location = line.find("\t");
+	peptide = line.substr(0, tab_location);
+	decoys.emplace_back(peptide, std::stod(line.substr(tab_location + 1, std::string::npos)));
+      }
+    }
+  }
+  targetFile.close();
+  decoyFile.close();
+  tdc_procedure(targets, decoys);
+  for(std::vector<match_t>::iterator target_it = targets.begin(); target_it != targets.end(); ++target_it){
+    std::cout << (*target_it).peptide << std::to_string((*target_it).q_value) << std::endl;
+  }
   return 0;
 }
-
