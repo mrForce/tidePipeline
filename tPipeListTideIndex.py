b491efc9e35dd493b9944505d6569c9187d95365
Jordan Force <jordan.force@uconn.edu>
2018-04-17 12:52:04 -0400

    Better listing of tide indices

--- tPipeListTideIndex.py
+++ tPipeListTideIndex.py
@@ -2,7 +2,7 @@ import tPipeDB
 import tPipeProject
 import argparse
 import sys
-
+import pprint
 parser = argparse.ArgumentParser(description='List Tide indices')
 
 parser.add_argument('project_folder', help='The location of the project folder')
@@ -18,6 +18,7 @@ project_folder = args.project_folder
 project = tPipeProject.Project(project_folder, ' '.join(sys.argv))
 project.begin_command_session()
 indices = project.get_tide_indices()
+printer = pprint.PrettyPrinter(indent=3)
 print('indices')
-print(indices)
+printer.pprint(indices)
 project.end_command_session()
