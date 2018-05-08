b87fa0354a4be3d3bd7b7d60fbd4b33adb2cf43e
Jordan Force <jordan.force@uconn.edu>
2018-05-08 12:05:53 -0400

    Ran percolator

---
+++ Errors.py
@@ -0,0 +1,188 @@
+class Error(Exception):
+    pass
+
+class NoSuchTargetSetError(Error):
+    def __init__(self, target_set_name):
+        self.target_set_name = target_set_name
+    def __repr__(self):
+        return 'There is no TargetSet with the name: ' + self.target_set_name
+
+
+class TargetSetNameMustBeUniqueError(Error):
+    def __init__(self, target_set_name):
+        self.target_set_name = target_set_name
+    def __repr__(self):
+        return 'There is already a TargetSet with the name: ' + self.target_set_name
+
+
+class NoSuchFilteredNetMHCError(Error):
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return 'There is no FilteredNetMHC entry with the name: ' + self.name
+
+class TideSearchRowDoesNotExistError(Error):
+    def __init__(self, tide_search_name):
+        self.tide_search_name = tide_search_name
+    def __repr__(self):
+        return 'There is no TideSearch row with the name: ' + str(self.tide_search_name)
+
+class AssignConfidenceNameMustBeUniqueError(Error):
+    def __init__(self, assign_confidence_name):
+        self.assign_confidence_name = assign_confidence_name
+    def __repr__(self):
+        return 'There is already an AssignConfidence row with the name: ' + str(self.assign_confidence_name)
+class PercolatorNameMustBeUniqueError(Error):
+    def __init__(self, percolator_name):
+        self.percolator_name = percolator_name
+    def __repr__(self):
+        return 'There is already a Percolator row with the name: ' + str(self.percolator_name)
+
+class AssignConfidenceFailedError(Error):
+    def __init__(self, command):
+        self.message = 'assign-confidence command: ' + command + ' failed'
+    def __repr__(self):
+        return self.message
+
+
+class PercolatorFailedError(Error):
+    def __init__(self, command):
+        self.message = 'percolator command: ' + command + ' failed'
+    def __repr__(self):
+        return self.message
+
+class MGFFileMissingError(Error):
+    def __init__(self, idMGFfile, name, path):
+        self.message = 'MGF file with ID: ' + str(idMGFfile) + ' and name: ' + name + ' and path: ' + path + ' is missing'
+    def __repr__(self):
+        return self.message
+    
+class ProjectPathAlreadyExistsError(Error):
+    def __init__(self, project_folder):
+        self.message = 'Project folder ' + project_folder + ' already exists.'
+    def __repr__(self):
+        return self.message
+class FASTAWithNameAlreadyExistsError(Error):
+    def __init__(self, name):
+        self.message = 'There\'s already a FASTA file with the name: ' + name + ' in the database'
+    def __repr__(self):
+        return self.message
+class FileDoesNotExistError(Error):
+    def __init__(self, filepath):
+        self.message = 'File with path: ' + filepath + ' does not exist'
+    def __repr__(self):
+        return self.message
+class FASTAWithNameDoesNotExistError(Error):
+    def __init__(self, name):
+        self.message = 'There is no FASTA entry with the name: ' + name
+
+    def __repr__(self):
+        return self.message
+class HLAWithNameExistsError(Error):
+    def __init__(self, hla_name):
+        self.message = 'There is already an HLA with the name: ' + hla_name
+    def __repr__(self):
+        return self.message
+class NoSuchSpeciesError(Error):
+    def __init__(self, species_name):
+        self.message = 'There is no species with the name: ' + species_name
+    def __repr__(self):
+        return self.message
+class MultipleSpeciesWithSameNameError(Error):
+    def __init__(self, species_name):
+        self.message = 'There are multiple species with the name: ' + species_name
+    def __repr__(self):
+        return self.message
+class NoSpeciesWithNameError(Error):
+    def __init__(self, species_name):
+        self.message = 'There are no species with the name: ' + species_name
+    def __repr__(self):
+        return self.message
+class NoSpeciesWithIDError(Error):
+    def __init__(self, species_id):
+        self.message = 'There are no species with the ID: ' + str(species_id)
+    def __repr__(self):
+        return self.message
+class MultipleSpeciesWithSameIDError(Error):
+    def __init__(self, species_id):
+        self.message = 'There are multiple species with the ID: ' + species_id
+    def __repr__(self):
+        return self.message
+
+class NotProperFASTAFileError(Error):
+    def __init__(self, path):
+        self.message = 'The file at found at: ' + path + ' could not be parsed as a FASTA file'
+    def __repr__(self):
+        return self.message
+class FASTAMissingError(Error):
+    def __init__(self, fasta_id, fasta_name, path):
+        self.message = 'The row in the FASTA table, with id: ' + str(fasta_id) + ' and name: ' + str(fasta_name) + ' points to a FASTA file with path: ' + path + ', but this file does not exist.'
+    def __repr__(self):
+        return self.message
+
+class OperationsLockedError(Error):
+    def __init__(self):
+        self.message = 'The operation lock file (operation_lock) exists, so we cannot do anything with this project at the moment'
+    def __repr__(self):
+        return self.message
+class NoSuchPeptideListError(Error):
+    def __init__(self, peptide_list_name):
+        self.message = 'There is no row in the PeptideList table with the name: ' + peptide_list_name
+
+    def __repr__(self):
+        return self.message
+
+class NoSuchHLAError(Error):
+    def __init__(self, hla_name):
+        self.message = 'There is no HLA with the name: ' + hla_name
+    def __repr__(self):
+        return self.message
+
+class PeptideListFileMissingError(Error):
+    def __init__(self, peptide_list_id, peptide_list_name, path):
+        self.message = 'The row in the PeptideList table, with id: ' + str(peptide_list_id) + ' and name: ' + str(peptide_list_name) + ' points to a peptide file with path: ' + path + ', but this file does not exist.'
+    def __repr__(self):
+        return self.message
+
+class MGFRowDoesNotExistError(Error):
+    def __init__(self, name):
+        self.message = 'There is no MGF file with the name: ' + name
+
+    def __repr__(self):
+        return self.message
+class MGFNameMustBeUniqueError(Error):
+    def __init__(self, name):
+        self.message = 'There\'s already an MGF file with the name: ' + name
+
+    def __repr__(self):
+        return self.message
+    
+class TideIndexFailedError(Error):
+    def __init__(self, command):
+        self.message = 'The tide-search command ended: ' + command + ' with non-zero exit code'
+    def __repr__(self):
+        return self.message
+
+
+class NoSuchTideIndexError(Error):
+    def __init__(self, name):
+        self.message = 'There is no tide index with the name: ' + name
+    def __repr__(self):
+        return self.message
+class TideSearchFailedError(Error):
+    def __init__(self, command):
+        self.message = 'The tide-search command ended: ' + command + ' with non-zero exit code'
+    def __repr__(self):
+        return self.message
+
+class TideSearchNameMustBeUniqueError(Error):
+    def __init__(self, name):
+        self.message = 'There is already a Tide Search with the name: ' + name
+    def __repr__(self):
+        return self.message
+
+class InvalidParameterLineError(Error):
+    def __init__(self, line):
+        self.message = 'The line does not follow the format for a percolator parameter file: ' + line
+    def __repr__(self):
+        return self.message
--- tPipeProject.py
+++ tPipeProject.py
@@ -14,293 +14,10 @@ from datetime import datetime
 import json
 import TargetSetSourceCount
 import ReportGeneration
-CRUX_BINARY = '/usr/bin/crux'
-class Error(Exception):
-    pass
-
-class NoSuchTargetSetError(Error):
-    def __init__(self, target_set_name):
-        self.target_set_name = target_set_name
-    def __repr__(self):
-        return 'There is no TargetSet with the name: ' + self.target_set_name
-
-
-class TargetSetNameMustBeUniqueError(Error):
-    def __init__(self, target_set_name):
-        self.target_set_name = target_set_name
-    def __repr__(self):
-        return 'There is already a TargetSet with the name: ' + self.target_set_name
-
-
-class NoSuchFilteredNetMHCError(Error):
-    def __init__(self, name):
-        self.name = name
-    def __repr__(self):
-        return 'There is no FilteredNetMHC entry with the name: ' + self.name
-
-class TideSearchRowDoesNotExistError(Error):
-    def __init__(self, tide_search_name):
-        self.tide_search_name = tide_search_name
-    def __repr__(self):
-        return 'There is no TideSearch row with the name: ' + str(self.tide_search_name)
-
-class AssignConfidenceNameMustBeUniqueError(Error):
-    def __init__(self, assign_confidence_name):
-        self.assign_confidence_name = assign_confidence_name
-    def __repr__(self):
-        return 'There is already an AssignConfidence row with the name: ' + str(self.assign_confidence_name)
-
-class AssignConfidenceFailedError(Error):
-    def __init__(self, command):
-        self.message = 'assign-confidence command: ' + command + ' failed'
-    def __repr__(self):
-        return self.message
+from Errors import *
+from Runners import *
     
-class MGFFileMissingError(Error):
-    def __init__(self, idMGFfile, name, path):
-        self.message = 'MGF file with ID: ' + str(idMGFfile) + ' and name: ' + name + ' and path: ' + path + ' is missing'
-    def __repr__(self):
-        return self.message
     
-class ProjectPathAlreadyExistsError(Error):
-    def __init__(self, project_folder):
-        self.message = 'Project folder ' + project_folder + ' already exists.'
-    def __repr__(self):
-        return self.message
-class FASTAWithNameAlreadyExistsError(Error):
-    def __init__(self, name):
-        self.message = 'There\'s already a FASTA file with the name: ' + name + ' in the database'
-    def __repr__(self):
-        return self.message
-class FileDoesNotExistError(Error):
-    def __init__(self, filepath):
-        self.message = 'File with path: ' + filepath + ' does not exist'
-    def __repr__(self):
-        return self.message
-class FASTAWithNameDoesNotExistError(Error):
-    def __init__(self, name):
-        self.message = 'There is no FASTA entry with the name: ' + name
-
-    def __repr__(self):
-        return self.message
-class HLAWithNameExistsError(Error):
-    def __init__(self, hla_name):
-        self.message = 'There is already an HLA with the name: ' + hla_name
-    def __repr__(self):
-        return self.message
-class NoSuchSpeciesError(Error):
-    def __init__(self, species_name):
-        self.message = 'There is no species with the name: ' + species_name
-    def __repr__(self):
-        return self.message
-class MultipleSpeciesWithSameNameError(Error):
-    def __init__(self, species_name):
-        self.message = 'There are multiple species with the name: ' + species_name
-    def __repr__(self):
-        return self.message
-class NoSpeciesWithNameError(Error):
-    def __init__(self, species_name):
-        self.message = 'There are no species with the name: ' + species_name
-    def __repr__(self):
-        return self.message
-class NoSpeciesWithIDError(Error):
-    def __init__(self, species_id):
-        self.message = 'There are no species with the ID: ' + str(species_id)
-    def __repr__(self):
-        return self.message
-class MultipleSpeciesWithSameIDError(Error):
-    def __init__(self, species_id):
-        self.message = 'There are multiple species with the ID: ' + species_id
-    def __repr__(self):
-        return self.message
-
-class NotProperFASTAFileError(Error):
-    def __init__(self, path):
-        self.message = 'The file at found at: ' + path + ' could not be parsed as a FASTA file'
-    def __repr__(self):
-        return self.message
-class FASTAMissingError(Error):
-    def __init__(self, fasta_id, fasta_name, path):
-        self.message = 'The row in the FASTA table, with id: ' + str(fasta_id) + ' and name: ' + str(fasta_name) + ' points to a FASTA file with path: ' + path + ', but this file does not exist.'
-    def __repr__(self):
-        return self.message
-
-class OperationsLockedError(Error):
-    def __init__(self):
-        self.message = 'The operation lock file (operation_lock) exists, so we cannot do anything with this project at the moment'
-    def __repr__(self):
-        return self.message
-class NoSuchPeptideListError(Error):
-    def __init__(self, peptide_list_name):
-        self.message = 'There is no row in the PeptideList table with the name: ' + peptide_list_name
-
-    def __repr__(self):
-        return self.message
-
-class NoSuchHLAError(Error):
-    def __init__(self, hla_name):
-        self.message = 'There is no HLA with the name: ' + hla_name
-    def __repr__(self):
-        return self.message
-
-class PeptideListFileMissingError(Error):
-    def __init__(self, peptide_list_id, peptide_list_name, path):
-        self.message = 'The row in the PeptideList table, with id: ' + str(peptide_list_id) + ' and name: ' + str(peptide_list_name) + ' points to a peptide file with path: ' + path + ', but this file does not exist.'
-    def __repr__(self):
-        return self.message
-
-class MGFRowDoesNotExistError(Error):
-    def __init__(self, name):
-        self.message = 'There is no MGF file with the name: ' + name
-
-    def __repr__(self):
-        return self.message
-class MGFNameMustBeUniqueError(Error):
-    def __init__(self, name):
-        self.message = 'There\'s already an MGF file with the name: ' + name
-
-    def __repr__(self):
-        return self.message
-    
-class TideIndexFailedError(Error):
-    def __init__(self, command):
-        self.message = 'The tide-search command ended: ' + command + ' with non-zero exit code'
-    def __repr__(self):
-        return self.message
-
-
-class NoSuchTideIndexError(Error):
-    def __init__(self, name):
-        self.message = 'There is no tide index with the name: ' + name
-    def __repr__(self):
-        return self.message
-class TideSearchFailedError(Error):
-    def __init__(self, command):
-        self.message = 'The tide-search command ended: ' + command + ' with non-zero exit code'
-    def __repr__(self):
-        return self.message
-
-class TideSearchNameMustBeUniqueError(Error):
-    def __init__(self, name):
-        self.message = 'There is already a Tide Search with the name: ' + name
-    def __repr__(self):
-        return self.message
-
-
-    
-class TideSearchRunner:
-    def __init__(self, tide_search_options):
-        self.tide_search_options = tide_search_options
-    @staticmethod
-    def get_tide_search_options():
-        return {'--mod-precision': {'type':int}, '--auto-precursor-window': {'choices': ['false', 'warn', 'fail']}, '--max-precursor-charge': {'type': int}, '--precursor-window': {'type': float}, '--precursor-window-type': {'choices': ['mass', 'mz', 'ppm']}, '--auto-mz-bin-width': {'choices': ['false', 'warn', 'fail']}, '--compute-sp': {'choices': ['T', 'F']}, '--deisotope': {'type': float}, '--exact-p-value': {'choices':['T', 'F']}, '--isotope-error': {'type': str}, '--min-peaks': {'type': int}, '--mz-bin-offset': {'type': float}, '--mz-bin-width': {'type': float}, '--peptide-centric-search': {'choices': ['T', 'F']}, '--score-function': {'choices': ['xcorr', 'residue-evidence', 'both']}, '--fragment-tolerance': {'type': float}, '--evidence-granularity': {'type': int}, '--remove-precursor-peak': {'choices': ['T', 'F']}, '--remove-precursor-tolerance': {'type': float}, '--scan-number': {'type': str}, '--skip-processing': {'choices': ['T', 'F']}, '--spectrum-charge': {'choices': ['1', '2', '3', 'all']}, '--spectrum-max-mz': {'type': float}, '--spectrum-min-mz': {'type': float}, '--use-flanking-peaks': {'choices': ['T', 'F']}, '--use-neutral-loss-peaks': {'choices': ['T', 'F']}, '--num-threads': {'type': int}, '--pm-charge': {'type': int}, '--pm-max-frag-mz': {'type': float}, '--pm-max-precursor-delta-ppm': {'type': float}, '--pm-max-precursor-mz': {'type': float}, '--pm-max-scan-seperation': {'type': int}, '--pm-min-common-frag-peaks': {'type': int}, '--pm-min-frag-mz': {'type': float}, '--pm-min-peak-pairs': {'type': int}, '--pm-min-precursor-mz': {'type': float}, '--pm-min-scan-frag-peaks': {'type': int}, '--pm-pair-top-n-frag-peaks': {'type': int}, '--pm-top-n-frag-peaks': {'type': int}, '--concat': {'choices': ['T', 'F']}, '--file-column': {'choices': ['T', 'F']}, '--fileroot': {'type': str}, '--mass-precision': {'type': int}, '--mzid-output': {'choices': ['T', 'F']}, '--precision': {'type': int}, '--spectrum-parser': {'choices': ['pwiz', 'mstoolkit']}, '--store-spectra': {'type': str}, '--top-match': {'type': int}, '--use-z-line': {'choices': ['T', 'F']}}
-
-    #change the options here
-    def run_search_create_row(self, mgf_row, index_row, output_directory_tide, output_directory_db, options, project_path, tide_search_row_name):        
-        """ First, decide the filename """
-        param_filename = str(uuid.uuid4().hex) + '-param.txt'
-        while os.path.isfile(os.path.join(project_path, 'tide_param_files', param_filename)):
-            param_filename = str(uuid.uuid4().hex) + '-param.txt'
-        with open(os.path.join(project_path, 'tide_param_files', param_filename), 'w') as f:        
-            for k, v in options.items():
-                f.write(k + '=' + str(v) + '\n')
-        spectra_file = os.path.join(project_path, mgf_row.MGFPath)
-        index_filename = os.path.join(project_path, index_row.TideIndexPath)
-        print('current working directory: ' + os.getcwd())
-        command = [CRUX_BINARY, 'tide-search', '--output-dir', output_directory_tide, '--parameter-file', os.path.join(project_path, 'tide_param_files', param_filename), spectra_file, index_filename]
-        try:
-            p = subprocess.call(command, stdout=sys.stdout, stderr=sys.stderr)
-        except subprocess.CalledProcessError:
-            raise TideSearchFailedError(' '.join(command))
-        search_row = tPipeDB.TideSearch(idTideIndex = index_row.idTideIndex, idMGF=mgf_row.idMGFfile, targetPath=os.path.join(output_directory_db, 'tide-search.target.txt'), decoyPath=os.path.join(output_directory_db, 'tide-search.decoy.txt'), paramsPath=os.path.join('tide_param_files', param_filename), logPath=os.path.join(output_directory_db, 'tide-search.log.txt'), TideSearchName=tide_search_row_name)
-        return search_row
-
-        
-class TideIndexRunner:
-    def __init__(self, tide_index_options):
-        #tide_index_options is a dictionary.
-        self.tide_index_options = tide_index_options
-
-
-    @staticmethod
-    def get_tide_index_options():
-        return {'--clip-nterm-methionine': {'choices':['T', 'F']}, '--isotopic-mass': {'choices': ['average', 'mono']}, '--max-length': {'type': int}, '--max-mass': {'type':str}, '--min-length': {'type':int}, '--min-mass': {'type':str}, '--cterm-peptide-mods-spec': {'type': str}, '--max-mods': {'type': int}, '--min-mods': {'type':int}, '--mod-precision': {'type':int}, '--mods-spec': {'type': str}, '--nterm-peptide-mods-spec': {'type':str}, '--allow-dups': {'choices': ['T', 'F']}, '--decoy-format': {'choices': ['none', 'shuffle', 'peptide-reverse', 'protein-reverse']}, '--decoy-generator': {'type':str}, '--keep-terminal-aminos': {'choices': ['N', 'C', 'NC', 'none']}, '--seed': {'type':str}, '--custom-enzyme': {'type': str}, '--digestion': {'choices': ['full-digest', 'partial-digest', 'non-specific-digest']}, '--enzyme': {'choices': ['no-enzyme', 'trypsin', 'trypsin/p', 'chymotrypsin', 'elactase', 'clostripain', 'cyanogen-bromide', 'iodosobenzoat', 'proline-endopeptidase', 'staph-protease', 'asp-n', 'lys-c', 'lys-n', 'arg-c', 'glu-c', 'pepsin-a', 'elastase-trypsin-chymotrypsin', 'custom-enzyme']}, '--missed-cleavages': {'type': int}}
-    @staticmethod
-    def convert_cmdline_option_to_column_name(option):
-        converter = {'clip-nterm-methionine': 'clip_nterm_methionine', 'isotopic-mass': 'isotopic_mass', 'max-length': 'max_length', 'max-mass': 'max_mass', 'min-length': 'min_length', 'min-mass': 'min_mass', 'cterm-peptide-mods-spec': 'cterm_peptide_mods_spec', 'max-mods': 'max_mods', 'min-mods':'min_mods', 'mod-precision': 'mod_precision', 'mods-spec': 'mods_spec', 'nterm-peptide-mods-spec': 'nterm_peptide_mods_spec', 'allow-dups': 'allow_dups', 'decoy-format': 'decoy_format', 'decoy-generator': 'decoy_generator', 'keep-terminal-aminos': 'keep_terminal_aminos', 'seed': 'seed', 'custom-enzyme': 'custom_enzyme', 'digestion': 'digestion', 'enzyme': 'enzyme', 'missed-cleavages': 'missed_cleavages'}
-        if option in converter:
-            return converter[option]
-        else:
-            return None
-    def run_index_create_row(self, fasta_path, output_directory_tide, output_directory_db, index_filename):
-        #first, need to create the tide-index command
-        command = [CRUX_BINARY, 'tide-index']
-        for k,v in self.tide_index_options.items():
-            if k and v:
-                command.append('--' + k)
-                command.append(v)
-        command.append('--output-dir')
-        command.append(output_directory_tide)
-        command.append(fasta_path)
-        command.append(os.path.join(output_directory_tide, index_filename))
-        print('command')
-        print(' '.join(command))
-        try:
-            p = subprocess.call(command, stdout=sys.stdout, stderr=sys.stderr)
-        except subprocess.CalledProcessError:
-            raise TideIndexFailedError(' '.join(command))
-        column_arguments = {}
-        for k, v in self.tide_index_options.items():
-            column_name = TideIndexRunner.convert_cmdline_option_to_column_name(k)
-            if column_name:
-                column_arguments[column_name] = v
-        column_arguments['TideIndexPath'] = os.path.join(output_directory_db, index_filename)
-        return tPipeDB.TideIndex(**column_arguments)
-
-class AssignConfidenceRunner:
-    def __init__(self, assign_confidence_options):
-        self.assign_confidence_options = assign_confidence_options
-
-
-
-    @staticmethod
-    def get_assign_confidence_options():
-        return {'--estimation-method': {'choices': ['mix-max', 'tdc', 'peptide-level']}, '--score': {'type': str}, '--sidak': {'choices': ['T', 'F']}, '--top-match-in': {'type': int}, '--combine-charge-states': {'choices': ['T', 'F']}, '--combine-modified-peptides': {'choices': ['T', 'F']}}
-
-    @staticmethod
-    def convert_cmdline_option_to_column_name(option):
-        converter = {'estimation-method': 'estimation_method', 'score': 'score', 'sidak': sidak, 'top-match-in': 'top_match_in', 'combine-charge-states': 'combine_charge_states', 'combine-modified-peptides': 'combine_modified_peptides'}
-        if option in converter:
-            return converter[option]
-        else:
-            return None
-
-    def run_assign_confidence_create_row(self, target_path, output_directory_tide, output_directory_db, assign_confidence_name, tide_search_row):
-        #first, need to create the tide-index command
-        command = [CRUX_BINARY, 'assign-confidence']
-        for k,v in self.assign_confidence_options.items():
-            if k and v:
-                command.append('--' + k)
-                command.append(v)
-        command.append('--output-dir')
-        command.append(output_directory_tide)
-        command.append(target_path)
-
-        print('command')
-        print(' '.join(command))
-        try:
-            p = subprocess.call(command, stdout=sys.stdout, stderr=sys.stderr)
-        except subprocess.CalledProcessError:
-            raise AssignConfidenceFailedError(' '.join(command))
-        column_arguments = {}
-        for k, v in self.assign_confidence_options.items():
-            column_name = AssignConfidenceRunner.convert_cmdline_option_to_column_name(k)
-            if column_name:
-                column_arguments[column_name] = v
-        column_arguments['AssignConfidenceOutputPath'] = output_directory_db
-        column_arguments['AssignConfidenceName'] = assign_confidence_name
-        column_arguments['tideSearch'] = tide_search_row
-        return tPipeDB.AssignConfidence(**column_arguments)
 
 
 class Project:
@@ -451,6 +168,25 @@ class Project:
                 raise TideSearchRowDoesNotExistError(tide_search_name)
             if assign_confidence_row:
                 raise AssignConfidenceNameMustBeUniqueError(assign_confidence_name)
+
+    def percolator(self, tide_search_name, percolator_runner, percolator_name):
+        tide_search_row = self.db_session.query(tPipeDB.TideSearch).filter_by(TideSearchName = tide_search_name).first()
+        percolator_row = self.db_session.query(tPipeDB.Percolator).filter_by(PercolatorName = percolator_name).first()
+        if tide_search_row and (percolator_row is None):
+            target_path = os.path.join(self.project_path, tide_search_row.targetPath)
+            output_directory_name = str(uuid.uuid4().hex)
+            while os.path.isdir(os.path.join(self.project_path, 'percolator_results', output_directory_name)):
+                output_directory_name = str(uuid.uuid4().hex)
+            output_directory_tide = os.path.join(self.project_path, 'percolator_results', output_directory_name)
+            output_directory_db = os.path.join('percolator_results', output_directory_name)
+            new_row = percolator_runner.run_percolator_create_row(target_path, output_directory_tide, output_directory_db, percolator_name, tide_search_row)
+            self.db_session.add(new_row)
+            self.db_session.commit()
+        else:
+            if tide_search_row is None:
+                raise TideSearchRowDoesNotExistError(tide_search_name)
+            if percolator_row:
+                raise PercolatorNameMustBeUniqueError(percolator_name)
 
     def list_tide_search(self, mgf_name = None, tide_index_name = None):
         """
