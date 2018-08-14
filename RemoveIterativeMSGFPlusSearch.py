57c74999450fb2e2b348f6eb5f9db3eb247fe783
Jordan Force <jordan.force@uconn.edu>
2018-08-14 12:00:22 -0400

    Changed the message for RemoveIterativeMSGFPlusSearch.py

--- RemoveIterativeMSGFPlusSearch.py
+++ RemoveIterativeMSGFPlusSearch.py
@@ -3,11 +3,11 @@ import argparse
 import sys
 import os
 import PostProcessing
-parser = argparse.ArgumentParser(description='Run an iterative MSGF+ Search')
+parser = argparse.ArgumentParser(description='Remove an MSGF+ iterative search')
 
 parser.add_argument('project_folder', help='The location of the project folder')
 
-parser.add_argument('name', help='The name of the iterative MSGF+ Search')
+parser.add_argument('name', help='The name of the iterative MSGF+ Search to remove')
 
 
 args = parser.parse_args()
@@ -19,6 +19,7 @@ print('project folder: ' + project_folder)
 project = PostProcessing.PostProcessing(project_folder, ' '.join(sys.argv))
 
 project.begin_command_session()
+
 row = project.get_msgfplusiterativesearch_row(args.name)
 remover = RowRemoval.RowRemoval(project.db_session, row, project_folder)
 message = remover.get_deletion_message()
