58d8c96db21629abb8cff76fe5985e4bdfae0add
Jordan Force <jordan.force@uconn.edu>
2018-09-05 10:38:47 -0400

    going to change scripts

--- health_center_data/build_index.sh
+++ health_center_data/build_index.sh
@@ -1,19 +1,28 @@
-python3 CreateTargetSet.py HealthCenter CombinedTwoPercent --FilteredNetMHC MouseProteinsEightMersKbTwoPercent --FilteredNetMHC MouseProteinsNineMersKbTwoPercent --FilteredNetMHC MouseProteinsTenMersKbTwoPercent --FilteredNetMHC MouseProteinsElevenMersKbTwoPercent --FilteredNetMHC MouseProteinsEightMersDbTwoPercent --FilteredNetMHC MouseProteinsNineMersDbTwoPercent --FilteredNetMHC MouseProteinsTenMersDbTwoPercent --FilteredNetMHC MouseProteinsElevenMersDbTwoPercent 
+cd ..
 
-python3 CreateTideIndex.py HealthCenter TargetSet CombinedTwoPercent CombinedTwoPercentTideIndex --custom-enzyme '[Z]|[Z]' --enzyme custom-enzyme --contaminant_set JeremyContaminants
+HealthCenter=/data1/jordan/PipelineProjects/HealthCenter
 
-python3 CreateMSGFPlusIndex.py HealthCenter TargetSet CombinedTwoPercent CombinedTwoPercentMSGFIndex --contaminant_set JeremyContaminants --memory 30000
+#python3 CreateTargetSet.py $HealthCenter CombinedTwoPercent --FilteredNetMHC MouseProteinsEightMersKbTwoPercent --FilteredNetMHC MouseProteinsNineMersKbTwoPercent --FilteredNetMHC MouseProteinsTenMersKbTwoPercent --FilteredNetMHC MouseProteinsElevenMersKbTwoPercent --FilteredNetMHC MouseProteinsEightMersDbTwoPercent --FilteredNetMHC MouseProteinsNineMersDbTwoPercent --FilteredNetMHC MouseProteinsTenMersDbTwoPercent --FilteredNetMHC MouseProteinsElevenMersDbTwoPercent 
 
-python3 CreateTargetSet.py HealthCenter CombinedOnePercent --FilteredNetMHC MouseProteinsEightMersKbOnePercent --FilteredNetMHC MouseProteinsNineMersKbOnePercent --FilteredNetMHC MouseProteinsTenMersKbOnePercent --FilteredNetMHC MouseProteinsElevenMersKbOnePercent --FilteredNetMHC MouseProteinsEightMersDbOnePercent --FilteredNetMHC MouseProteinsNineMersDbOnePercent --FilteredNetMHC MouseProteinsTenMersDbOnePercent --FilteredNetMHC MouseProteinsElevenMersDbOnePercent 
+#python3 CreateTideIndex.py $HealthCenter TargetSet CombinedTwoPercent CombinedTwoPercentTideIndex --custom-enzyme '[Z]|[Z]' --enzyme custom-enzyme --contaminant_set JeremyContaminants
 
-python3 CreateTideIndex.py HealthCenter TargetSet CombinedOnePercent CombinedOnePercentTideIndex --custom-enzyme '[Z]|[Z]' --enzyme custom-enzyme --contaminant_set JeremyContaminants
+python3 CreateTideIndex.py $HealthCenter TargetSet CombinedTwoPercent CombinedTwoPercentTideIndexReverseForDecoys --custom-enzyme '[Z]|[Z]' --enzyme custom-enzyme --contaminant_set JeremyContaminants --decoy-format peptide-reverse
+#python3 CreateMSGFPlusIndex.py $HealthCenter TargetSet CombinedTwoPercent CombinedTwoPercentMSGFIndex --contaminantSet JeremyContaminants --memory 30000
 
-python3 CreateMSGFPlusIndex.py HealthCenter TargetSet CombinedOnePercent CombinedOnePercentMSGFIndex --contaminant_set JeremyContaminants --memory 30000
+#python3 CreateTargetSet.py $HealthCenter CombinedOnePercent --FilteredNetMHC MouseProteinsEightMersKbOnePercent --FilteredNetMHC MouseProteinsNineMersKbOnePercent --FilteredNetMHC MouseProteinsTenMersKbOnePercent --FilteredNetMHC MouseProteinsElevenMersKbOnePercent --FilteredNetMHC MouseProteinsEightMersDbOnePercent --FilteredNetMHC MouseProteinsNineMersDbOnePercent --FilteredNetMHC MouseProteinsTenMersDbOnePercent --FilteredNetMHC MouseProteinsElevenMersDbOnePercent 
 
-python3 CreateTargetSet.py HealthCenter Combined --PeptideList MouseProteinNineMers --PeptideList MouseProteinEightMers --PeptideList MouseProteinTenMers --PeptideList MouseProteinElevenMers
+#python3 CreateTideIndex.py $HealthCenter TargetSet CombinedOnePercent CombinedOnePercentTideIndex --custom-enzyme '[Z]|[Z]' --enzyme custom-enzyme --contaminant_set JeremyContaminants
 
-python3 CreateTideIndex.py HealthCenter TargetSet Combined CombinedTideIndex --custom-enzyme '[Z]|[Z]' --enzyme custom-enzyme --contaminant_set JeremyContaminants
+python3 CreateTideIndex.py $HealthCenter TargetSet CombinedOnePercent CombinedOnePercentTideIndexReverseForDecoys --custom-enzyme '[Z]|[Z]' --enzyme custom-enzyme --contaminant_set JeremyContaminants --decoy-format peptide-reverse
 
-python3 CreateMSGFPlusIndex.py HealthCenter TargetSet CombinedOnePercent CombinedMSGFIndex --contaminant_set JeremyContaminants --memory 30000
+#python3 CreateMSGFPlusIndex.py $HealthCenter TargetSet CombinedOnePercent CombinedOnePercentMSGFIndex --contaminantSet JeremyContaminants --memory 30000
+
+#python3 CreateTargetSet.py $HealthCenter Combined --PeptideList MouseProteinNineMers --PeptideList MouseProteinEightMers --PeptideList MouseProteinTenMers --PeptideList MouseProteinElevenMers
+
+#python3 CreateTideIndex.py $HealthCenter TargetSet Combined CombinedTideIndex --custom-enzyme '[Z]|[Z]' --enzyme custom-enzyme --contaminant_set JeremyContaminants
+
+python3 CreateTideIndex.py $HealthCenter TargetSet Combined CombinedTideIndexReverseForDecoys --custom-enzyme '[Z]|[Z]' --enzyme custom-enzyme --contaminant_set JeremyContaminants --decoy-format peptide-reverse
+
+#python3 CreateMSGFPlusIndex.py $HealthCenter TargetSet Combined CombinedMSGFIndexTwo --contaminantSet JeremyContaminants --memory 30000
 
 
--- health_center_data/export.sh
+++ health_center_data/export.sh
@@ -1,47 +1,86 @@
+
 cd ..
+export HealthCenter=/data1/jordan/PipelineProjects/HealthCenter
+NAME="CombinedTwoPercentTide_Prep_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
 
-NAME="CombinedTwoPercentTide_Prep_percolator_filtered"
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
+NAME="CombinedOnePercentTide_Prep_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
 
-NAME="CombinedOnePercentTide_Prep_percolator_filtered"
+NAME="CombinedTide_Prep_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
 
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
-
-NAME="CombinedTide_Prep_percolator_filtered"
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
-
-NAME="CombinedTwoPercentTide_Run2_percolator_filtered"
-
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
-
-
-NAME="CombinedOnePercentTide_Run2_percolator_filtered"
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
-
-NAME="CombinedTide_Run2_percolator_filtered"
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
-
-NAME="CombinedTwoPercentMSGF_Prep_percolator_filtered"
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
+NAME="CombinedTwoPercentTide_Run2_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
 
 
-NAME="CombinedOnePercentMSGF_Prep_percolator_filtered"
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
+NAME="CombinedOnePercentTide_Run2_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
+
+NAME="CombinedTide_Run2_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
+
+NAME="CombinedTwoPercentMSGF_Prep_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
 
 
-NAME="CombinedMSGF_Run2_percolator_filtered"
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
+NAME="CombinedOnePercentMSGF_Prep_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
 
 
-NAME="CombinedOnePercentMSGF_Run2_percolator_filtered"
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
+NAME="CombinedMSGF_Run2Two_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
 
 
-NAME="CombinedTwoPercentMSGF_Run2_percolator_filtered"
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
+NAME="CombinedOnePercentMSGF_Run2_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
+
+
+NAME="CombinedTwoPercentMSGF_Run2_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
 
 
 
-NAME="CombinedMSGF_Prep_percolator_filtered"
-python3 ExportPeptides.py HealthCenter FilteredSearchResult $(NAME) health_center_data/$(NAME)/peptides.txt --contaminants health_center_data/$(NAME)/contaminants.txt
+NAME="CombinedMSGF_PrepTwo_percolator_filtered_five_percent_fdr_three"
+#mkdir health_center_data/$NAME
+#python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
+
+
+NAME="CombinedTwoPercentTide_Prep_percolator_with_param_five_percent_fdr"
+mkdir health_center_data/$NAME
+python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
+
+NAME="CombinedOnePercentTide_Prep_percolator_with_param_five_percent_fdr"
+mkdir health_center_data/$NAME
+python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
+
+NAME="CombinedTide_Prep_percolator_with_param_five_percent_fdr"
+mkdir health_center_data/$NAME
+python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
+
+
+
+
+NAME="CombinedTwoPercentTide_PrepReverseForDecoys_percolator_five_percent_fdr"
+mkdir health_center_data/$NAME
+python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
+
+NAME="CombinedOnePercentTide_PrepReverseForDecoys_percolator_five_percent_fdr"
+mkdir health_center_data/$NAME
+python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
+
+NAME="CombinedTide_PrepReverseForDecoys_percolator_five_percent_fdr"
+mkdir health_center_data/$NAME
+python3 ExportPeptides.py $HealthCenter FilteredSearchResult $NAME health_center_data/$NAME/peptides.txt --contaminants health_center_data/$NAME/contaminants.txt
 
