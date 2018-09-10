8a573a30a860ebd3d42c596118817361c11b7ff4
Jordan Force <jordan.force@uconn.edu>
2018-09-10 11:47:41 -0400

    Added a script (along with an awk program) for merging NetMHC run results, and updated the netmhc_decoys.sh script to only output the decoys

---
+++ bash_scripts/merge_netmhc_runs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+chainJoin(){
+
+    if [ "$#" -eq 1 ]; then
+	sort -t, -k1 $1
+    else
+	join -t "," <(sort -t, -k1 $1) <( sort -t, -k1 <( chainJoin  ${@:2} ) )
+    fi
+}
+
+
+if [ $# -lt 4 ]; then
+    echo "Usage: merge_netmhc_runs.sh mode output netmhc_run_1 netmhc_run_2...\n where mode is 0 if we want to randomly select the rank to use; if 1, then select lowest (best) rank; if 2 then select greatest (worst) rank"
+else
+    fileArgs=${@:3: $#-1}
+    joined=$(mktemp)
+    chainJoin $fileArgs > $joined
+    awk -F "," -f select_netmhc_rank.awk $joined $1 
+fi
