ed8b8def89b7cd823989912d77ed51093d907b89
Jordan Force <jordan.force@uconn.edu>
2018-09-24 12:18:12 -0400

    I changed the way decoys are constructed from alternative NetMHC alleles

--- BashScripts.py
+++ BashScripts.py
@@ -25,6 +25,14 @@ def extract_peptides_with_length(location, length):
     output_location = outs.strip()
     assert(os.path.isfile(output_location))
     return output_location
+def num_lines(file_path):
+    proc = subprocess.Popen(['grep', '-c', '-v', '^ *$', file_path], stdout=subprocess.PIPE)
+    try:
+        outs, errors = proc.communicate(timeout=240)
+    except:
+        assert(False)
+    output = outs.strip().decode('utf-8')
+    return int(output.split(' ')[0])
 
 
 def merge_netmhc_runs(mode, netmhc_runs):
@@ -38,11 +46,22 @@ def merge_netmhc_runs(mode, netmhc_runs):
     assert(os.path.isfile(output_location))
     return output_location
 
-def call_netmhc_decoys(input_location, targets):
+def call_netmhc_decoys(input_location, target, num_decoys):
     #returns the output location (which is a temporary file)
-    proc = subprocess.Popen(['bash_scripts/netmhc_decoys.sh', input_location, targets], stdout=subprocess.PIPE)
+    proc = subprocess.Popen(['bash_scripts/netmhc_decoys.sh', input_location, target, str(num_decoys)], stdout=subprocess.PIPE)
     try:
         outs, errors = proc.communicate(timeout=600)
+    except:
+        assert(False)
+    output_location = outs.strip()
+    assert(os.path.isfile(output_location))
+    return output_location
+
+def call_merge_and_sort(files):
+    #files should be a list of file paths
+    proc = subprocess.Popen(['bash_scripts/merge_and_sort.sh', *files], stdout=subprocess.PIPE)
+    try:
+        outs, errors = proc.communicate(timeout=240)
     except:
         assert(False)
     output_location = outs.strip()
@@ -61,29 +80,15 @@ parsed_netmhc_objects must be a list of ParsedNetMHC instances
 Finish these later
 """
 def netMHCDecoys(parsed_netmhc_objects, target_location, output_location, *, merge_mode = 0):
-    lengths = line_length_set(target_location)
-    netmhc_length_dict = collections.defaultdict(list)
-    for x in parsed_netmhc_objects:
-        netmhc_length_dict[x.get_length()] = x.get_location()
-    temp_outputs = []
-    for length, locations in netmhc_length_dict.items():
-        """
-        If there are multple locations, merge the runs using merge_netmhc_runs.sh
+    merged_decoy_candidates = call_merge_and_sort([x[1] for x in parsed_netmhc_objects.items()])
+    num_target_lines = num_lines(target_location)
+    decoy_location = call_netmhc_decoys(merged_decoy_candidates, target_location, num_target_lines)
 
-        Then, extract the targets with the given length
-
-        Then, call netmhc_decoys.sh
-        """
-        if len(locations) > 1:
-            locations = [merge_netmhc_runs(merge_mode, locations)]
-        targets_with_length = extract_peptides_with_length(target_location, length)
-        temp_output_location = call_netmhc_decoys(locations[0], targets_with_length)
-        temp_outputs.append(temp_output_location)
     with tempfile.NamedTemporaryFile() as f:
-        join_peptides_to_fasta(temp_outputs, f.name, 'XXX_')
-        for temp_output in temp_outputs:
-            os.remove(temp_output)
+        join_peptides_to_fasta(decoy_location, f.name, 'XXX_')
+        os.remove(decoy_location)
         combine_files([output_location, f.name], output_location)
+    
    
     
     
--- bash_scripts/netmhc_decoys.sh
+++ bash_scripts/netmhc_decoys.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-#$1 is the parsed NetMHC output, while $2 are the target peptides
-netmhc_peptides=$(mktemp)
-decoy_candidates=$(mktemp)
-sorted_netmhc=$(mktemp)
-peptides_no_fasta_headers=$(mktemp)
-output_location=$(mktemp)
-sort -t , -k1 $1 > $sorted_netmhc
-cat  $sorted_netmhc | awk -F "," '{print $1}' > $netmhc_peptides
-grep -v -F ">" $2 | sort > $peptides_no_fasta_headers
-comm -23 $netmhc_peptides $peptides_no_fasta_headers > $decoy_candidates
-if [[ $(wc -l < $decoy_candidates) -lt $(wc -l < $peptides_no_fasta_headers) ]]; then
-    exit 200;
-fi
-join -t "," $sorted_netmhc $decoy_candidates | sort -n -t , -k2 | awk -F "," '{print $1}' | head -n $(wc -l < $peptides_no_fasta_headers) > $output_location
-echo $output_location
-rm $netmhc_peptides $decoy_candidates $sorted_netmhc $peptides_no_fasta_headers
-#rm $netmhc_peptides $decoy_candidates $sorted_netmhc
-#awk '{print">XXX_"NR; print $0}' $temp_output > $3
-#awk '{print">"NR; print $0}' $peptides_no_fasta_headers >> $3
-#rm  $peptides_no_fasta_headers $temp_output
+#$1 is the combined parsed NetMHC output for the decoy candidates, while $2 are the target peptides, and $3 is the # of decoys needed
+
+
+#first, we need to sort both alphabetically.
+sorted_targets=$(mktemp)
+alphabetically_sorted_decoy_candidates=$(mktemp)
+decoy_candidates_no_targets=$(mktemp)
+output=$(mktemp)
+sort $2 > $sorted_targets
+sort -t, -k1 $1 > $alphabetically_sorted_decoy_candidates
+#remove any target duplicates from the decoys.
+join -1 1 -t, -v1 $alphabeticall_sorted_decoy_candidates $sorted_targets > $decoy_candidates_no_targets
+#resort by IC50, then take head 
+head -n $3 <(sort -t, -k2 $decoy_candidates_no_targets) > $output
