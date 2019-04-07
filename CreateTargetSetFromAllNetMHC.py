bb39e8085c71ddde91e0c88fabaaf1fee6162148
Jordan Force <jordan.force@uconn.edu>
2019-04-07 08:32:16 -0400

    thing

--- CreateTargetSetFromAllNetMHC.py
+++ CreateTargetSetFromAllNetMHC.py
@@ -4,7 +4,7 @@ import argparse
 import DB
 import sys
 import os
-parser = argparse.ArgumentParser(description='Create a TargetSet from all of the FilteredNetMHCs in the project (I mainly added this for use in Galaxy')
+parser = argparse.ArgumentParser(description='Create a TargetSet from all of the FilteredNetMHCs in the project (I mainly added this for use in Galaxy)')
 
 parser.add_argument('project_folder', help='The location of the project folder')
 
@@ -20,6 +20,8 @@ header, results = project.get_list_filtered_netmhc()
 
 filtered_netmhc = []
 for x in results:
+    print('result')
+    print(x)
     filtered_netmhc.append(x['ID'])
 
 project.add_targetset(filtered_netmhc, [], args.TargetSetName)
