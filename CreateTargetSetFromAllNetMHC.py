4e6be6858f8859096302c8ab244fd39a72401a6f
Jordan Force <jordan.force@uconn.edu>
2019-04-07 08:30:27 -0400

    Added a new script

---
+++ CreateTargetSetFromAllNetMHC.py
@@ -0,0 +1,27 @@
+import Base
+
+import argparse
+import DB
+import sys
+import os
+parser = argparse.ArgumentParser(description='Create a TargetSet from all of the FilteredNetMHCs in the project (I mainly added this for use in Galaxy')
+
+parser.add_argument('project_folder', help='The location of the project folder')
+
+parser.add_argument('TargetSetName', help='Name of the TargetSet')
+
+args = parser.parse_args()
+project_folder = args.project_folder
+print('project folder: ' + project_folder)
+project = Base.Base(project_folder, ' '.join(sys.argv))
+project.begin_command_session()
+
+header, results = project.get_list_filtered_netmhc()
+
+filtered_netmhc = []
+for x in results:
+    filtered_netmhc.append(x['ID'])
+
+project.add_targetset(filtered_netmhc, [], args.TargetSetName)
+
+project.end_command_session()
