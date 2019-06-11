78490b05a8172dd5bd79ac454d66ea437b239bee
Jordan Force <jordan.force@uconn.edu>
2019-06-11 16:06:21 -0400

    Started a utility to export PIN for Ellie

---
+++ ExportElliePIN.py
@@ -0,0 +1,29 @@
+import Base
+import PostProcessing
+import argparse
+import sys
+import os
+import DB
+parser = argparse.ArgumentParser(description='Export clean PIN files for search, with NetMHC scores appended to specified alleles.')
+
+parser.add_argument('project_folder', help='The location of the project folder')
+
+parser.add_argument('search')
+parser.add_argument('positive_fdr')
+parser.add_argument('positives_pin')
+parser.add_argument('unknown_pin')
+parser.add_argument('--allele', help='Name of an allele to run the matches against with NetMHC', action='append')
+
+
+args = parser.parse_args()
+
+project_folder = args.project_folder
+
+
+
+project = Base.Base(project_folder, ' '.join(sys.argv))
+project.begin_command_session()
+row = project.db_session.query(DB.MSGFPlusSearch).filter_by(SearchName = name).first()
+print('row')
+print(row)            
+project.end_command_session()
