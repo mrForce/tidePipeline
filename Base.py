58b20475b3e7312e779f785eda3e54507365fc34
Jordan Force <jordan.force@uconn.edu>
2019-10-14 20:12:51 -0400

    Whoops, introduced a syntax error to Base.py

--- Base.py
+++ Base.py
@@ -248,7 +248,6 @@ class Base:
         return self.db_session.query(DB.FilteredNetMHC).filter_by(FilteredNetMHCName = name).first()
     def get_netmhc_row(self, name):
         return self.db_session.query(DB.NetMHC).filter_by(Name=name).first()
-    def get_netmhc_row(self, allele, length):
         
     def get_target_set_row(self, name):
         return self.db_session.query(DB.TargetSet).filter_by(TargetSetName = name).first()
